c8f707c94358fd1f76051c540be445216410c541
りんめ <rinmesk@yahoo.com>
2022-04-19 02:14:39 +0700

    Navbar Changed + Tooltip

--- pages/_app.js
+++ pages/_app.js
@@ -4,27 +4,41 @@ import Image from 'next/image'
 
 function RinApp({ Component, pageProps }) {
   return (
-  <>
+    <>
       {nav()}
-  <Component {...pageProps} />
-  </>
+      <Component {...pageProps} />
+    </>
   );
 
   function nav() {
+    /*Navbar Start*/
+    const NavStart = <div class="navbar-start flex-none space-y-10">
+      <div class="tooltip tooltip-bottom" data-tip="bruh">
+        <label class="btn-ghost btn swap swap-rotate">
+          <input type="checkbox" />
+          <svg class="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" /></svg>
+          <svg class="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" /></svg>
+        </label>
+      </div>
+    </div>;
+
+    /*Navbar Center*/
+    const NavCenter = <div class="navbar-center">
+      <Link href="/"><a class="btn btn-ghost normal-case text-xl font-bold tracking-widest">りんめ</a></Link>
+    </div>;
+
+    /*Navbar End*/
+    const NavEnd = <div class="navbar-end">
+      <button class="btn btn-square btn-ghost">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+      </button>
+    </div>;
+
+    /* Return Navbar */
     return <div class="navbar bg-base-100">
-      <div class="flex-none">
-        <button class="btn btn-square btn-ghost">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-        </button>
-      </div>
-      <div class="flex-1">
-        <a class="btn btn-ghost normal-case text-xl">りんめ</a>
-      </div>
-      <div class="flex-none">
-        <button class="btn btn-square btn-ghost">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
-        </button>
-      </div>
+      {NavStart}
+      {NavCenter}
+      {NavEnd}
     </div>
   };
 }
