ec68215b6c2553e59aa07d507fa8f683d509e13a
りんめ <rinmesk@yahoo.com>
2022-04-19 05:41:15 +0700

    Renamed

--- pages/api/F.js
+++
@@ -1,6 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-  res.status(404).json({ eh: 'not found what are you finding?' })
-}
