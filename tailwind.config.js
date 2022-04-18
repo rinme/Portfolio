70631ccd2ca85c28fb995c83671b9c3dc0a0046d
りんめ <rinmesk@yahoo.com>
2022-04-19 02:14:01 +0700

    More plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,25 @@
 module.exports = {
   daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "rintheme",
     themes: [
       {
         rintheme: {
-        "primary": "#fbcfe8",       
-        "secondary": "#93c5fd",
-        "accent": "#a5b4fc",
-        "neutral": "#374151",
-        "base-100": "#1c1917",
-        "info": "#7dd3fc",
-        "success": "#99f6e4",
-        "warning": "#fef08a",
-        "error": "#f87171",
+          "primary": "#fbcfe8",
+          "secondary": "#93c5fd",
+          "accent": "#a5b4fc",
+          "neutral": "#fda4af",
+          "base-100": "#1c1917",
+          "info": "#7dd3fc",
+          "success": "#99f6e4",
+          "warning": "#fef08a",
+          "error": "#f87171",
         },
       },
     ],
@@ -20,5 +28,9 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require("daisyui"),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
