f92029021d0507a6d1f44ea60a8f3525f29b725c
りんめ <rinmesk@yahoo.com>
2022-08-13 18:23:34 +0700

    Fixed themes curse something wried

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,29 +8,18 @@ module.exports = {
     rtl: false,
     prefix: "",
     darkTheme: "rintheme",
-    themes: [
-      {
-        rintheme: {
-          "primary": "#fbcfe8",
-          "secondary": "#93c5fd",
-          "accent": "#a5b4fc",
-          "neutral": "#fda4af",
-          "base-100": "#1c1917",
-          "info": "#7dd3fc",
-          "success": "#99f6e4",
-          "warning": "#fef08a",
-          "error": "#f87171",
-        },
-      },
-    ],
+    themes: [],
   },
-  content: ['./src/pages/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [
     require("daisyui"),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio'),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
   ],
-}
+};
