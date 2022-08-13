04db83fe06368e823e4ff30c57d538742e57d11d
りんめ <rinmesk@yahoo.com>
2022-08-13 23:47:15 +0700

    Animation! - Developing

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,20 @@
+import { AnimatePresence } from "framer-motion";
+import { useTheme } from "next-themes";
 import "../styles/globals.css";
 import dynamic from "next/dynamic";
 import Layout from "../components/layout";
 
 function RinApp({ Component, pageProps }) {
+  const { theme, setTheme } = useTheme();
   return (
     <Layout>
-      <Component {...pageProps} />
+      <AnimatePresence
+        exitBeforeEnter
+        initial={false}
+        onExitComplete={() => window.scrollTo(0, 0)}
+      >
+        <Component {...pageProps} />
+      </AnimatePresence>
     </Layout>
   );
 }
