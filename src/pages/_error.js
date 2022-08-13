87b54d0c524ade8ef64442aa34c378b6c406bca4
りんめ <rinmesk@yahoo.com>
2022-08-13 18:27:30 +0700

    Multiple errors support

---
+++ src/pages/_error.js
@@ -0,0 +1,29 @@
+import Link from "next/link";
+function Error({ statusCode }) {
+  return (
+    <>
+      <div className="flex justify-center">
+        <p>
+          {statusCode
+            ? `An error ${404} : Page cannot be found`
+            : "An error occurred on client"}
+        </p>
+      </div>
+      <div className="flex justify-center">
+        You can press <kbd class="kbd kbd-sm">R</kbd> to retry. or
+      </div>
+      <div className="flex justify-center">
+        <Link href="/">
+          <button class="btn btn-error">Back to Home</button>
+        </Link>
+      </div>
+    </>
+  );
+}
+
+Error.getInitialProps = ({ res, err }) => {
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
+};
+
+export default Error;
