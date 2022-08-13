43faa7221db31345c3bb3cb31032bfb61877044a
りんめ <rinmesk@yahoo.com>
2022-08-13 23:45:29 +0700

    Moved header from layout

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header>
+      <div class="navbar bg-neutral text-neutral-content">
+        <a class="btn btn-ghost normal-case text-xl">Rin</a>
+      </div>
+    </header>
+  );
+};
+export default Header;
