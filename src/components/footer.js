598865580aa94627668e1a8b833a57bec7a5b6cb
りんめ <rinmesk@yahoo.com>
2022-08-13 23:45:06 +0700

    Footer!!!

---
+++ src/components/footer.js
@@ -0,0 +1,8 @@
+const Footer = () => {
+    return (
+      <div className="flex justify-center  bottom-0 absolute left-2/4 -translate-x-1/2">
+        <p className="font-extrabold">&copy; 2022 - Rinme</p>
+      </div>
+    );
+  };
+  export default Footer;
