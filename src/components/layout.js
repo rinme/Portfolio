666484bf1b887c52b78fb3700d25925a5c973123
りんめ <rinmesk@yahoo.com>
2022-08-14 16:42:01 +0700

    Revamp!

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { useRouter } from "next/router";
 import { motion } from "framer-motion";
 import Header from "../components/header";
@@ -29,43 +28,19 @@ export default function Layout({ children }) {
 
   return (
     <>
-      <div className="min-h-screen flex flex-col">
-        <Header />
-        <div className="flex flex-col md:flex-row flex-1">
-          <div className="drawer">
-            <input id="nav-drawer" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content">
-              <label for="nav-drawer" className="btn btn-primary drawer-button">
-                =
-              </label>
-            </div>
-            <div className="drawer-side w-80">
-              <label for="nav-drawer" className="drawer-overlay w-80"></label>
-              <ul className="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
-                {menuItems.map(({ href, title }) => (
-                  <li className="m-2" key={title}>
-                    <Link href={href}>
-                      <a>{title}</a>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-          <motion.main
-            initial="hidden"
-            animate="enter"
-            exit="exit"
-            variants={variants}
-            transition={{ type: "easeIn" }}
-            className="
+      <Header />
+      <motion.main
+        initial="hidden"
+        animate="enter"
+        exit="exit"
+        variants={variants}
+        transition={{ type: "easeIn" }}
+        className="
                     flex-1
                 "
-          >
-            {children}
-          </motion.main>
-        </div>
-      </div>
+      >
+        {children}
+      </motion.main>
     </>
   );
 }
