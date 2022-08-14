f1c3144f2514095359525f8ac7198e31a9390282
りんめ <rinmesk@yahoo.com>
2022-08-14 16:42:14 +0700

    No themes Changer

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,16 @@
 import { AnimatePresence } from "framer-motion";
-import { useTheme } from "next-themes";
 import "../styles/globals.css";
 import dynamic from "next/dynamic";
 import Layout from "../components/layout";
+import Footer from "../components/footer";
 
 function RinApp({ Component, pageProps }) {
-  const { theme, setTheme } = useTheme();
   return (
     <Layout>
-      <AnimatePresence
-        exitBeforeEnter
-        initial={false}
-        onExitComplete={() => window.scrollTo(0, 0)}
-      >
+      <AnimatePresence exitBeforeEnter initial={false}>
         <Component {...pageProps} />
       </AnimatePresence>
+      <Footer />
     </Layout>
   );
 }
