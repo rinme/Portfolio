6a5eb0a3a5deb57c0069ca72558a155ecc7b268f
りんめ <rinmesk@yahoo.com>
2022-08-14 16:42:32 +0700

    Just for visual testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,29 @@ export default function Home() {
     <div className="flex h-full flex-col justify-center items-center">
       <h1 className="text-4xl mb-5 font-bold">Home</h1>
       <span className="text-7xl">🏡</span>
+      <span className="text-7xl">🏡</span>
+      <span className="text-7xl">🏡</span>
+      <span className="text-7xl">🏡</span>
+      <span className="text-7xl">🏡</span>
+      <span className="text-7xl">🏡</span>
+      <span className="text-7xl">🏡</span>
+      <span className="text-7xl">🏡</span>
+      <span className="text-7xl">🏡</span>
+      <span className="text-7xl">🏡</span>
+      <span className="text-7xl">🏡</span>
+      <span className="text-7xl">🏡</span>
+      <span className="text-7xl">🏡</span>
+      <span className="text-7xl">🏡</span>
+      <span className="text-7xl">🏡</span>
+      <span className="text-7xl">🏡</span>
+      <span className="text-7xl">🏡</span>
+      <span className="text-7xl">🏡</span>
+      <span className="text-7xl">🏡</span>
+      <span className="text-7xl">🏡</span>
+      <span className="text-7xl">🏡</span>
+      <span className="text-7xl">🏡</span>
+      <span className="text-7xl">🏡</span>
+      <span className="text-7xl">🏡</span>
     </div>
   );
 }
